d9c729f9362e8cc5899f2bdb5e0b133c5bbda130
Andrii <A.I.Khmelovsky@gmail.com>
2021-07-18 12:41:00 +0300

    translate to russian

--- src/routes/Projects/components/ReferralChart/ReferralChart.jsx
+++ src/routes/Projects/components/ReferralChart/ReferralChart.jsx
@@ -1,92 +1,96 @@
-import React, { useState, useEffect } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import styles from './ReferralChart.styles'
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import styles from './ReferralChart.styles';
 import { Line } from 'react-chartjs-2';
 import { useSelector } from 'react-redux';
 import { getFormattedDate, getOccurrence } from 'utils/misc';
 
-const useStyles = makeStyles(styles)
+const useStyles = makeStyles(styles);
 
 const options = {
-    maintainAspectRatio: false,
-    scales: {
-        yAxes: [
-            {
-                ticks: {
-                    beginAtZero: true,
-                },
-            },
-        ],
-    },
+	maintainAspectRatio: false,
+	scales: {
+		yAxes: [
+			{
+				ticks: {
+					beginAtZero: true,
+				},
+			},
+		],
+	},
 };
 
 function ReferralChart() {
-    const classes = useStyles()
+	const classes = useStyles();
 
-    const profile = useSelector(({ firebase: { profile } }) => profile)
+	const profile = useSelector(({ firebase: { profile } }) => profile);
 
-    const [data, setData] = useState({
-        labels: [],
-        datasets: [
-            {
-                label: 'Referrals',
-                data: [],
-                fill: false,
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgba(255, 99, 132, 0.2)',
-            },
-        ],
-    })
+	const [data, setData] = useState({
+		labels: [],
+		datasets: [
+			{
+				label: 'Реффералы',
+				data: [],
+				fill: false,
+				backgroundColor: 'rgb(255, 99, 132)',
+				borderColor: 'rgba(255, 99, 132, 0.2)',
+			},
+		],
+	});
 
-    useEffect(() => {
-        if (profile?.level1?.length >= 0 && profile?.level2?.length >= 0 && profile?.level3?.length >= 0) {
-            createChartData()
-        }
-    }, [profile])
+	useEffect(() => {
+		if (
+			profile?.level1?.length >= 0 &&
+			profile?.level2?.length >= 0 &&
+			profile?.level3?.length >= 0
+		) {
+			createChartData();
+		}
+	}, [profile]);
 
-    const createChartData = () => {
-        const tempData = []
+	const createChartData = () => {
+		const tempData = [];
 
-        const completeArray = [...profile.level1, ...profile.level2, ...profile.level3]
-            .map(item => item.createdAt.seconds)
-            .sort((a, b) => a - b)
-            .map(seconds => getFormattedDate(seconds * 1000))
+		const completeArray = [
+			...profile.level1,
+			...profile.level2,
+			...profile.level3,
+		]
+			.map((item) => item.createdAt.seconds)
+			.sort((a, b) => a - b)
+			.map((seconds) => getFormattedDate(seconds * 1000));
 
-        const filteredArray = [...new Set(completeArray)]
+		const filteredArray = [...new Set(completeArray)];
 
-        console.log(completeArray, filteredArray)
+		console.log(completeArray, filteredArray);
 
-        for (let item of filteredArray) {
+		for (let item of filteredArray) {
+			tempData.push(getOccurrence(completeArray, item));
+		}
 
-            tempData.push(getOccurrence(completeArray, item))
-        }
+		setData({
+			labels: filteredArray,
+			datasets: [
+				{
+					label: 'Реффералы',
+					data: tempData,
+					fill: false,
+					backgroundColor: '#02AA44',
+					borderColor: '#02AA4488',
+				},
+			],
+		});
+	};
 
-        setData({
-            labels: filteredArray,
-            datasets: [
-                {
-                    label: 'Referrals',
-                    data: tempData,
-                    fill: false,
-                    backgroundColor: '#02AA44',
-                    borderColor: '#02AA4488',
-                },
-            ],
-        })
-
-    }
-
-    return (
-        <div style={{ height: 300 }}>
-            <Line data={data} options={options} />
-        </div>
-    )
+	return (
+		<div style={{ height: 300 }}>
+			<Line data={data} options={options} />
+		</div>
+	);
 }
 
-ReferralChart.propTypes = {
-}
+ReferralChart.propTypes = {};
 
-ReferralChart.defaultProps = {
-}
+ReferralChart.defaultProps = {};
 
-export default ReferralChart
+export default ReferralChart;
