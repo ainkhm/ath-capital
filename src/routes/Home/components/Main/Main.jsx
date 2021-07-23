3ce4915f73f0884857f9204c9a240a4110ab264c
Andrii <A.I.Khmelovsky@gmail.com>
2021-07-23 18:34:24 +0300

    text added

--- src/routes/Home/components/Main/Main.jsx
+++ src/routes/Home/components/Main/Main.jsx
@@ -1,40 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Main() {
+	return (
+		<section className='main'>
+			<div className='main-header'>
+				<h1 className='heading'>
+					ATH capital. <br />
+					Доход сегодня
+				</h1>
+				<p>Ваши деньги должны работать и мы вам в этом поможем</p>
 
-  return (
-    <section className="main">
-      <div className="main-header">
-        <h1 className="heading">ATH capital. Доход сегодня</h1>
-        <p>
-          Познакомьтесь с платежной платформой, созданной для любого бизнеса и
-          любого пути клиента.
-        </p>
-        <a href="#" className="btn-primary">
-          Начни свое путешествие с нами
-        </a>
-      </div>
-      <div className="main-video">
-        <video
-          video__selfhosted
-          className="video__selfhosted"
-          autoPlay
-          muted
-          playsInline
-          loop
-          webkitallowfullscreen
-          mozallowfullscreen
-          allowFullScreen
-          width="100%"
-        >
-          <source src="assets/anthem.mp4" type="video/mp4" />
-          Your browser does not support the video tag
-        </video>
-      </div>
-    </section>
-
-  )
+				<a href='#' className='btn-primary'>
+					Записаться на консультацию
+				</a>
+			</div>
+			<div className='main-video'>
+				<video
+					video__selfhosted
+					className='video__selfhosted'
+					autoPlay
+					muted
+					playsInline
+					loop
+					webkitallowfullscreen
+					mozallowfullscreen
+					allowFullScreen
+					width='100%'
+				>
+					<source src='assets/anthem.mp4' type='video/mp4' />
+					Your browser does not support the video tag
+				</video>
+			</div>
+		</section>
+	);
 }
 
-export default Main
+export default Main;
